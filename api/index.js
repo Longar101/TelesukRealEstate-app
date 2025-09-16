7101594385050c5dd252ab5253d6b40fa52126a6
LongarAker <LongarAker@gmail.com>
2025-09-16 09:25:02 +0300

    created the serever

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 
 
-//create server port
+//Express server creation  using install express js module 
+//we initialise app varible as object
 const app = express();
  app.listen(3000,()=>{
     console.log("server up and running ");
