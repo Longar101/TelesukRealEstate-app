57223508835ff445341da3f7ed66d18261b53f7b
LongarAker <LongarAker@gmail.com>
2025-09-16 12:55:30 +0300

    feat: Integrate MongoDB with Mongoose and dotenv for environment variable management

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
+//used promise asyncy await
+async function Database() {
+   try {
+           //used mongoose to connect create dbs connection
+           await mongoose.connect(process.env.MONGO);
+           console.log("database up and running successfully");
+           
+   } catch (error) {
+         console.log(error);
+         
+   }
+   
+}
+Database()
 
 
 
@@ -7,6 +24,8 @@ const express = require('express');
 //Express server creation  using install express js module 
 //we initialise app varible as object
 const app = express();
+     app.use(express.json());
+     app.use(cors);
  app.listen(3000,()=>{
     console.log("server up and running ");
     
