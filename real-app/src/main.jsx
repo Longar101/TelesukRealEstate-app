ed444be5725dda797a7f2501c9b80fc73003ce6d
LongarAker <LongarAker@gmail.com>
2025-09-20 12:54:47 +0300

    add redux pesist for localstorage cession remembarance

--- real-app/src/main.jsx
+++ real-app/src/main.jsx
@@ -14,16 +14,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { store } from './redux/store.js';
+import {persistor,store } from './redux/store.js';
 import {Provider} from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react';
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
- 
+ //debug
 
      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <App />
+        </PersistGate>
      </Provider>
   
  
--- real-app/src/redux/store.js
+++ real-app/src/redux/store.js
@@ -1,19 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import userReducer from './user/userSlice';
+import { persistReducer,persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 
 //global store
 
+const rootReducer = combineReducers({ user: userReducer })
+
+
+const persistConfig = {
+  key: "root",
+storage,// which storage we are using
+  version:1,// to manage the version of our state
+ 
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer:
-  {
-    user: userReducer// to access our user state in the component use useSelector((state)=>state.user)
-    
-  },
+  reducer: persistedReducer,
   
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false
-    })
+    }),
+
+
 });
+
+export const persistor = persistStore(store);// to create a persistor for our store
